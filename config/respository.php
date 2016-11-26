e32b9298a47a0deb8e330a7b1d506e899bb77d45
kenny Le <vanliem2204@gmail.com>
2016-11-27 00:14:08 +0700

    Init Project

---
+++ config/respository.php
@@ -0,0 +1,18 @@
+<?php
+return [
+	'generator'  => [
+        'basePath'      => app_path(),
+        'rootNamespace' => 'RuddyStore\\',
+        'paths'         => [
+            'models'       => 'Entities',
+            /*'repositories' => 'Repositories',
+            'interfaces'   => 'Repositories',
+            'transformers' => 'Transformers',
+            'presenters'   => 'Presenters',
+            'validators'   => 'Validators',
+            'controllers'  => 'Http/Controllers',
+            'provider'     => 'RepositoryServiceProvider',*/
+        ]
+    ]
+];
+?>
