c55909b3b8fbad816f703d8ad43af35bcfe1d52c
vagrant <vagrant@ruddystore>
2016-11-28 07:46:58 +0000

    Set new default namespace

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace RuddyStore\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
+use RuddyStore\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 
 class PasswordController extends Controller
